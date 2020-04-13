9cfe000f8d8eecc143eada0593364c9094c8d71a
Mateus Paixão <mateus.paixao@corebiz.com.br>
2020-04-13 09:40:44 -0300

    init fronent app, configure babel and webpack

---
+++ frontend/babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    presets:[
+        '@babel/preset-env', // converte código mais moderno para o mais antigo, baseado no ambiente
+        '@babel/preset-react' // adicionar funcionalidade do react (exemplo JSX)
+    ]
+}
